ebdbc10e4f3ed9513cf2e1c11460177d55efe4c8
DevEmad007 <FarshidIbtihaj@gmail.com>
2023-09-26 17:58:23 +0600

    footer add

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import FooterColumn from './FooterColumn';
+import { Link } from 'react-router-dom';
+import './footer.css';
 
 const Footer = () => {
+    const year = new Date().getFullYear();
     return (
         <footer className='footer'>
-            <FooterColumn />
-            <FooterColumn />
-            <FooterColumn />
-            <FooterColumn />
+            <div>
+                <FooterColumn />
+                <FooterColumn />
+                <FooterColumn />
+                <FooterColumn />
+            </div>
+            <div>
+                <div>
+                    <Link>Privacy Policy</Link>
+                    <Link>Terms and Conditions</Link>
+                </div>
+                <p>©{year} adidas America, Inc.</p>
+            </div>
         </footer>
     );
 };
