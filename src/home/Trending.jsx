3c0d234c38c89d67d214d064b4508ce73283b853
DevEmad007 <FarshidIbtihaj@gmail.com>
2023-10-09 17:40:29 +0600

    responsive ug fix

--- src/home/Trending.jsx
+++ src/home/Trending.jsx
@@ -1,33 +1,64 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import { Link } from 'react-router-dom/dist';
-import { tranding } from './db';
+import { tranding,newArrivlas } from './db';
 import './trending.css';
 import { FavoriteBorderOutlined } from '@mui/icons-material';
 
 const Trending = () => {
-    const one = tranding[ 0 ];
+    const [ linkOne,setLinkOne ] = useState(true);
 
     return (
         <>
             <section className="tranding">
-                <h2>What's Trending</h2>
+                <h2
+                    onClick={() => setLinkOne(true)}
+                    className={`newArrivals ${linkOne ? 'styleChangeOne' : ''}`}
+                >
+                    New Arrivals
+                </h2>
+                <h2
+                    onClick={() => setLinkOne(false)}
+                    className={`${linkOne ? '' : 'styleChangeOne'}`}
+                >
+                    What's Trending
+                </h2>
                 <div className="cartContainer">
-                    {tranding.map(item => (
-                        <Link key={item.id}>
-                            <div className='card' >
-                                <div className='cardImgContainer'>
-                                    <FavoriteBorderOutlined className='favIcon' />
-                                    <img src={item.img} alt="" />
-                                    <span className='priceTag'>${item.price}</span>
+                    {linkOne ?
+                        (newArrivlas.map(item => (
+                            <Link key={item.id}>
+                                <div className='card' >
+                                    <div className='cardImgContainer'>
+                                        <FavoriteBorderOutlined className='favIcon' />
+                                        <img src={item.img} alt="" />
+                                        <span className='priceTag'>${item.price}</span>
+                                    </div>
+                                    <div className="cardInfo">
+                                        <p> {item.title}</p>
+                                        <p>Orginals</p>
+                                        <p>Best Seller</p>
+                                    </div>
                                 </div>
-                                <div className="cardInfo">
-                                    <p> {item.title}</p>
-                                    <p>Orginals</p>
-                                    <p>Best Seller</p>
+                            </Link>
+                        )))
+                        :
+                        (tranding.map(item => (
+                            <Link key={item.id}>
+                                <div className='card' >
+                                    <div className='cardImgContainer'>
+                                        <FavoriteBorderOutlined className='favIcon' />
+                                        <img src={item.img} alt="" />
+                                        <span className='priceTag'>${item.price}</span>
+                                    </div>
+                                    <div className="cardInfo">
+                                        <p> {item.title}</p>
+                                        <p>Orginals</p>
+                                        <p>Best Seller</p>
+                                    </div>
                                 </div>
-                            </div>
-                        </Link>
-                    ))}
+                            </Link>
+                        )))
+                    }
+
                 </div>
             </section>
         </>
--- src/home/db.js
+++ src/home/db.js
@@ -54,3 +54,62 @@ export const tranding = [
         img: 'https://assets.adidas.com/images/w_280,h_280,f_auto,q_auto:sensitive/be101f424a2b4671944cf23a659a13de_9366/gazelle-shoes.jpg'
     }
 ];
+
+export const newArrivlas = [
+    {
+        id: 1,
+        title: "Men's Soccer White Argentina 22 3 Stars Home Authentic Jersey",
+        price: 150,
+        type: "Men's Soccer",
+        img: 'https://assets.adidas.com/images/w_280,h_280,f_auto,q_auto:sensitive/71df5608d75141ebaeb4056f43d02eca_9366/argentina-22-3-stars-home-authentic-jersey.jpg'
+    },
+    {
+        id: 3,
+        title: "Men's Originals Multi adidas Rivalry Low Itchy",
+        price: 70,
+        img: "https://assets.adidas.com/images/w_280,h_280,f_auto,q_auto:sensitive/7d004610770543a2bc9dfa66a959ee69_9366/adidas-rivalry-low-itchy.jpg"
+    },
+    {
+        id: 4,
+        title: "Women's Originals Multi adidas Originals x KSENIASCHNAIDER Fringed Shirt Dress",
+        price: 70,
+        img: "https://assets.adidas.com/images/w_280,h_280,f_auto,q_auto:sensitive/f76b00e73bb54c298ed0bf9ada71a931_9366/adidas-originals-x-kseniaschnaider-3-stripes-jeans.jpg"
+    },
+    {
+        id: 5,
+        title: "Women's Originals Turquoise Adifom",
+        price: 70,
+        img: 'https://assets.adidas.com/images/w_280,h_280,f_auto,q_auto:sensitive/566076ab3e444e31a1c31ace5cee21e1_9366/adifom-stan-smith-mule-shoes.jpg'
+    },
+    {
+        id: 6,
+        title: "Women's Originals Black adidas Originals x KSENIASCHNAIDER 3-Stripes Jeans",
+        price: 70,
+        img: "https://assets.adidas.com/images/w_280,h_280,f_auto,q_auto:sensitive/a485e8ba52284c26a19468d3f046d62b_9366/adidas-originals-x-kseniaschnaider-fringed-shirt-dress.jpg"
+    },
+    {
+        id: 2,
+        title: 'Originals Black Gazelle Bold Shoes',
+        price: 100,
+        type: "Orginals",
+        img: 'https://assets.adidas.com/images/w_280,h_280,f_auto,q_auto:sensitive/c60cd4a11b6945599e15a81a017fb25f_9366/samba-classic.jpg'
+    },
+    {
+        id: 7,
+        title: 'Originals Black Samba Decon Shoes',
+        price: 100,
+        img: 'https://assets.adidas.com/images/w_280,h_280,f_auto,q_auto:sensitive/97f86eede1374615a058a81700a27444_9366/gazelle-shoes.jpg'
+    },
+    {
+        id: 8,
+        title: 'Originals Black Samba Decon Shoes',
+        price: 100,
+        img: 'https://assets.adidas.com/images/w_280,h_280,f_auto,q_auto:sensitive/eb4fe70570824a5993f305aca11473de_9366/samba-shoes.jpg'
+    },
+    {
+        id: 9,
+        title: 'Originals Black Samba ADV Shoes',
+        price: 150,
+        img: 'https://assets.adidas.com/images/w_280,h_280,f_auto,q_auto:sensitive/be101f424a2b4671944cf23a659a13de_9366/gazelle-shoes.jpg'
+    }
+];
